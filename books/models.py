9c1e7f01d2b237a4a521f2bff7f4427f2d55afc3
Clinton Dreisbach <clinton@dreisbach.us>
2018-11-26 13:40:46 -0500

    Add Book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    description = models.TextField()
+    url = models.URLField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        if self.title and self.author:
+            return f"{self.title} by {self.author}"
+
+        return super().__str__()
